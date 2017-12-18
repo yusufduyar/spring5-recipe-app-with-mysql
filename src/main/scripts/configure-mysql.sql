a5c464917acc1dbb3de2560b965164d8d19a2345
yusufduyar <yusufduyar@outlook.com>
2017-12-18 17:24:55 +0300

    Mysql initial configurations added

---
+++ src/main/scripts/configure-mysql.sql
@@ -0,0 +1,28 @@
+#connect to mysql and run as root user
+#Create Databases
+CREATE DATABASE sfg_dev;
+CREATE DATABASE sfg_prod;
+
+#Create database service accounts
+CREATE USER 'sfg_dev_user'@'localhost' IDENTIFIED BY '123';
+CREATE USER 'sfg_prod_user'@'localhost' IDENTIFIED BY '123';
+CREATE USER 'sfg_dev_user'@'%' IDENTIFIED BY '123';
+CREATE USER 'sfg_prod_user'@'%' IDENTIFIED BY '123';
+
+#Database grants
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'%';
